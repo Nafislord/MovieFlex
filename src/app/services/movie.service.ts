1e0232a6c6bfec68c03eac0715de7eff5e80bc97
Nafislord <nafis123456789@gmail.com>
2019-08-29 13:50:42 +0100

    First Working API Call

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -18,7 +18,7 @@ export class MovieService {
   constructor(private http: HttpClient) { }
 
   searchData(title: string, type: SearchType): Observable<any> {
-    return this.http.get(`${this.url}?s=${encodeURI(title)}&type={type}&apikey=${this.apiKey}`)
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`)
       .pipe(
         map(results => {
           console.log('RAW: ', results);
