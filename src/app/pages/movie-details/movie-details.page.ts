b373343216cdb21a1d40d68b8c4cb1bb5a7cc2fd
Nafislord <nafis123456789@gmail.com>
2019-08-29 14:36:31 +0100

    Movie Details Page Added

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsPage implements OnInit {
 
-  constructor() { }
+  information = null; 
+
+  constructor(private activatedRoute: ActivatedRoute,private movieService: MovieService) { }
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.movieService.getDetails(id).subscribe(result=>{
+      console.log('details: ', result);
+      this.information = result;
+      console.log('details2: ', result);
+    });
+  }
+  openWebsite(){
+    window.open(this.information.Website,'_blank');
   }
 
 }
